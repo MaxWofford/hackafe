e0a03757da7256dc9ba5accd42b463051cc33b6a
MaxWofford <max.speed.wofford@gmail.com>
2014-08-19 01:07:22 -0700

    Added location marking with firebase

--- www/js/mark.js
+++ www/js/mark.js
@@ -14,8 +14,6 @@ function success(pos) {
   console.log('Your current position is:');
   console.log('Latitude : ' + latitude);
   console.log('Longitude: ' + longitude);
-  console.log('More or less ' + crd.accuracy + ' meters.');
-  $('#notification').removeClass('hidden').addClass('alert-success').text('Ready to mark at ' + crd.latitude + ', ' + crd.longitude);
 };
 
 function error(err) {
@@ -23,7 +21,6 @@ function error(err) {
   $('#notification').removeClass('hidden').text('ERROR(' + err.code + '): ' + err.message);
 };
 
-// onClick
 function findLocation() {
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(success, error, options);
@@ -33,19 +30,54 @@ function findLocation() {
   };
 };
 
+function updateLocation() {
+  latitude = map.getCenter().lat();
+  longitude = map.getCenter().lng();
+}
+// When user clicks "submit"
 function submitLocation() {
+  // Get user input for Firebase
   name = $('#nameInput').val();
   comments = $('#commentInput').val();
-  console.log('Before findLocation');
-  findLocation();
-  console.log('After findLocation');
-  if (latitude == longitude) {
-    $('#notification').removeClass('hidden').addClass('ERROR: Longitude and latitude = 0');
-  } else{
-    fb.push({name: name, comments: comments, lat: latitude, long: longitude});
+  //  Check if map.getCenter() data is valid, then update lat and lng
+  if ($.isNumeric(map.getCenter().lat() + map.getCenter().lng()) && map.getCenter().lat() + map.getCenter().lng() != 0) {
+    latitude = map.getCenter().lat();
+    longitude = map.getCenter().lng();
   };
+  // Push data to Firebase
+  fb.push({name: name, comments: comments, lat: latitude, lng: longitude});
 };
 
 function clearNotification() {
-	$('#notification').addClass('hidden').removeClass('alert-danger alert-warning alert-info alert-success').text('');
+  $('#notification').addClass('hidden').removeClass('alert-danger alert-warning alert-info alert-success').text('');
+};
+
+//Map related functions
+function createMap() {
+  // Generate map in #map
+  map = new GMaps({
+    div: '#map',
+    lat: latitude,
+    lng: longitude,
+    zoom: 19
+  });
+  // Hide loading button
+  $('#loader').addClass('hidden');
+  //Reveal the center-marker
+  $('#center-marker').removeClass('hidden');
+  $('#mark-button').removeClass('hidden');
+};
+
+function hideMap() {
+  $('#map').addClass('hidden');
+  $('#center-marker').addClass('hidden');
+  $('#mark-button').addClass('hidden');
+};
+
+function revealForm() {
+  hideMap();
+  clearNotification();
+  updateLocation();
+  $('#submit-form').removeClass('hidden');
+  $('#notification').removeClass('hidden').addClass('alert-success').text('Ready to mark at ' + latitude + ', ' + longitude);
 };

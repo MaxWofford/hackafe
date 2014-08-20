859af2f75997ee68fed8881d6fa0c7298e3be293
MaxWofford <max.speed.wofford@gmail.com>
2014-08-20 15:32:35 -0700

    Rewrote mark.js

--- www/js/mark.js
+++ www/js/mark.js
@@ -1,8 +1,10 @@
-// Initialization stuff
-var fb = new Firebase("https://hackafe.firebaseio.com/submit/");
+latitude = 0;
+longitude = 0;
+
+//Geolocation options and callbacks
 var options = {
   enableHighAccuracy: true,
-  timeout: 5000,
+  timeout: 50000, // While localhosting this is set to 50000
   maximumAge: 0
 };
 
@@ -14,6 +16,7 @@ function success(pos) {
   console.log('Your current position is:');
   console.log('Latitude : ' + latitude);
   console.log('Longitude: ' + longitude);
+  centerMap(latitude,longitude);
 };
 
 function error(err) {
@@ -21,63 +24,80 @@ function error(err) {
   $('#notification').removeClass('hidden').text('ERROR(' + err.code + '): ' + err.message);
 };
 
+//Geolocation function
 function findLocation() {
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(success, error, options);
-  } else{ //Geolocation not in navigator
-    console.warn('ERROR: Geolocation not found in navigator');
-    $('#notification').removeClass('hidden').text('ERROR: Geolocation not found in navigator');
+  } else{//Geolocation not in navigator
+    notifyAlert('Geolocation not found in navigator');
   };
+};
+
+//Map related functions
+function generateMap(lat,lng) {
+  map = new GMaps({
+    div: '#map',
+    lat: lat,
+    lng: lng,
+    zoom: 20
+  });
+};
+
+function centerMap(lat,lng) {
+  map.setCenter(lat, lng);
 };
 
 function updateLocation() {
   latitude = map.getCenter().lat();
   longitude = map.getCenter().lng();
-}
-// When user clicks "submit"
-function submitLocation() {
-  // Get user input for Firebase
-  name = $('#nameInput').val();
-  comments = $('#commentInput').val();
-  //  Check if map.getCenter() data is valid, then update lat and lng
-  if ($.isNumeric(map.getCenter().lat() + map.getCenter().lng()) && map.getCenter().lat() + map.getCenter().lng() != 0) {
-    latitude = map.getCenter().lat();
-    longitude = map.getCenter().lng();
-  };
-  // Push data to Firebase
-  fb.push({name: name, comments: comments, lat: latitude, lng: longitude});
+};
+
+//Firebase initialization
+var fb = new Firebase("https://hackafe.firebaseio.com/submit/");
+
+//Notification bar
+function notifyAlert(notifyMessage) {
+  clearNotification();//Clear previous notification
+  console.warn('ERROR: ' + errorMessage);
+  $('#notification').removeClass('hidden').addClass('alert-danger').text('ERROR: ' + notifyMessage);
+};
+
+function notifyInfo(notifyMessage) {
+  clearNotification();
+  console.warn('NOTIFY: ' + errorMessage);
+  $('#notification').removeClass('hidden').addClass('alert-info').text(notifyMessage);
 };
 
 function clearNotification() {
   $('#notification').addClass('hidden').removeClass('alert-danger alert-warning alert-info alert-success').text('');
 };
 
-//Map related functions
-function createMap() {
-  // Generate map in #map
-  map = new GMaps({
-    div: '#map',
-    lat: latitude,
-    lng: longitude,
-    zoom: 19
-  });
-  // Hide loading button
-  $('#loader').addClass('hidden');
-  //Reveal the center-marker
-  $('#center-marker').removeClass('hidden');
-  $('#mark-button').removeClass('hidden');
-};
-
-function hideMap() {
-  $('#map').addClass('hidden');
-  $('#center-marker').addClass('hidden');
-  $('#mark-button').addClass('hidden');
-};
-
+//Form related functions 
 function revealForm() {
-  hideMap();
-  clearNotification();
-  updateLocation();
-  $('#submit-form').removeClass('hidden');
-  $('#notification').removeClass('hidden').addClass('alert-success').text('Ready to mark at ' + latitude + ', ' + longitude);
+  updateLocation();//Get position from map
+  $('#map-container').addClass('hidden');//Hide map
+  $('#form').removeClass('hidden');//Reveal form
 };
+
+//Triggered when user clicks the final submit button
+function submitLocation() {
+  name = $('#nameInput').val();
+  comment = $('#commentInput').val();
+  fb.push({name: name, comment: comment, lat: latitude, lng: longitude});
+};
+
+//Triggered onLoad
+function startPage() {
+  $('#loader').addClass('hidden');//Hide loader button
+  $('#map-container').removeClass('hidden');//Reveal map
+  findLocation();//Find user current location
+  generateMap(0,0);//Generate a map without waiting for coordinates
+  map.setZoom(21);
+
+};
+
+//Triggered when user presses 'mark location'
+function markPressed() {
+  updateLocation();
+  revealForm();
+}

34c0b398960c91a1595f021df5ab0921592a4356
MaxWofford <max.speed.wofford@gmail.com>
2014-08-21 10:55:46 -0700

    Converted indentation to tabs

--- www/js/find.js
+++ www/js/find.js
@@ -1,2 +1,44 @@
-var fb = new Firebase("https://hackafe.firebaseio.com");
+// Example api request https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452
+
+var fb = new Firebase("https://hackafe.firebaseio.com/submit/");
+
+fb.on('child_added', function (snapshot) {
+	var cafeLocation = snapshot.val();
+	console.log("Latitude: " + cafeLocation.longitude);
+	console.log("Longitude: " + cafeLocation.latitude);
+});
+
+latitude = 0;
+longitude = 0;
+
+//Geolocation options and callbacks
+var options = {
+	enableHighAccuracy: true,
+	timeout: 50000, // While localhosting this is set to 50000
+	maximumAge: 0
+};
+
+function success(pos) {
+	console.log('finding location')
+	var crd = pos.coords;
+	latitude = crd.latitude;
+	longitude = crd.longitude;
+	console.log('Your current position is:');
+	console.log('Latitude : ' + latitude);
+	console.log('Longitude: ' + longitude);
+	centerMap(latitude,longitude);
+};
+
+function error(err) {
+	console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+//Geolocation function
+function findLocation() {
+	if ("geolocation" in navigator) {
+	navigator.geolocation.getCurrentPosition(success, error, options);
+	} else{//Geolocation not in navigator
+	console.log('Geolocation not found in navigator');
+	};
+};
 
--- www/js/mark.js
+++ www/js/mark.js
@@ -3,53 +3,53 @@ longitude = 0;
 
 //Geolocation options and callbacks
 var options = {
-  enableHighAccuracy: true,
-  timeout: 50000, // While localhosting this is set to 50000
-  maximumAge: 0
+	enableHighAccuracy: true,
+	timeout: 50000, // While localhosting this is set to 50000
+	maximumAge: 0
 };
 
 function success(pos) {
-  console.log('finding location')
-  var crd = pos.coords;
-  latitude = crd.latitude;
-  longitude = crd.longitude;
-  console.log('Your current position is:');
-  console.log('Latitude : ' + latitude);
-  console.log('Longitude: ' + longitude);
-  centerMap(latitude,longitude);
+	console.log('finding location')
+	var crd = pos.coords;
+	latitude = crd.latitude;
+	longitude = crd.longitude;
+	console.log('Your current position is:');
+	console.log('Latitude : ' + latitude);
+	console.log('Longitude: ' + longitude);
+	centerMap(latitude,longitude);
 };
 
 function error(err) {
-  console.warn('ERROR(' + err.code + '): ' + err.message);
-  $('#notification').removeClass('hidden').text('ERROR(' + err.code + '): ' + err.message);
+	console.warn('ERROR(' + err.code + '): ' + err.message);
+	$('#notification').removeClass('hidden').text('ERROR(' + err.code + '): ' + err.message);
 };
 
 //Geolocation function
 function findLocation() {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(success, error, options);
-  } else{//Geolocation not in navigator
-    notifyAlert('Geolocation not found in navigator');
-  };
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(success, error, options);
+	} else{//Geolocation not in navigator
+		notifyAlert('Geolocation not found in navigator');
+	};
 };
 
 //Map related functions
 function generateMap(lat,lng) {
-  map = new GMaps({
-    div: '#map',
-    lat: lat,
-    lng: lng,
-    zoom: 20
-  });
+	map = new GMaps({
+		div: '#map',
+		lat: lat,
+		lng: lng,
+		zoom: 20
+	});
 };
 
 function centerMap(lat,lng) {
-  map.setCenter(lat, lng);
+	map.setCenter(lat, lng);
 };
 
 function updateLocation() {
-  latitude = map.getCenter().lat();
-  longitude = map.getCenter().lng();
+	latitude = map.getCenter().lat();
+	longitude = map.getCenter().lng();
 };
 
 //Firebase initialization
@@ -57,47 +57,47 @@ var fb = new Firebase("https://hackafe.firebaseio.com/submit/");
 
 //Notification bar
 function notifyAlert(notifyMessage) {
-  clearNotification();//Clear previous notification
-  console.warn('ERROR: ' + errorMessage);
-  $('#notification').removeClass('hidden').addClass('alert-danger').text('ERROR: ' + notifyMessage);
+	clearNotification();//Clear previous notification
+	console.warn('ERROR: ' + errorMessage);
+	$('#notification').removeClass('hidden').addClass('alert-danger').text('ERROR: ' + notifyMessage);
 };
 
 function notifyInfo(notifyMessage) {
-  clearNotification();
-  console.warn('NOTIFY: ' + errorMessage);
-  $('#notification').removeClass('hidden').addClass('alert-info').text(notifyMessage);
+	clearNotification();
+	console.warn('NOTIFY: ' + errorMessage);
+	$('#notification').removeClass('hidden').addClass('alert-info').text(notifyMessage);
 };
 
 function clearNotification() {
-  $('#notification').addClass('hidden').removeClass('alert-danger alert-warning alert-info alert-success').text('');
+	$('#notification').addClass('hidden').removeClass('alert-danger alert-warning alert-info alert-success').text('');
 };
 
 //Form related functions 
 function revealForm() {
-  updateLocation();//Get position from map
-  $('#map-container').addClass('hidden');//Hide map
-  $('#form').removeClass('hidden');//Reveal form
+	updateLocation();//Get position from map
+	$('#map-container').addClass('hidden');//Hide map
+	$('#form').removeClass('hidden');//Reveal form
 };
 
 //Triggered when user clicks the final submit button
 function submitLocation() {
-  name = $('#nameInput').val();
-  comment = $('#commentInput').val();
-  fb.push({name: name, comment: comment, position: latitude + "," + longitude});
+	name = $('#nameInput').val();
+	comment = $('#commentInput').val();
+	fb.push({name: name, comment: comment, position: latitude + "," + longitude});
 };
 
 //Triggered onLoad
 function startPage() {
-  $('#loader').addClass('hidden');//Hide loader button
-  $('#map-container').removeClass('hidden');//Reveal map
-  findLocation();//Find user current location
-  generateMap(0,0);//Generate a map without waiting for coordinates
-  map.setZoom(21);
+	$('#loader').addClass('hidden');//Hide loader button
+	$('#map-container').removeClass('hidden');//Reveal map
+	findLocation();//Find user current location
+	generateMap(0,0);//Generate a map without waiting for coordinates
+	map.setZoom(21);
 
 };
 
 //Triggered when user presses 'mark location'
 function markPressed() {
-  updateLocation();
-  revealForm();
+	updateLocation();
+	revealForm();
 }

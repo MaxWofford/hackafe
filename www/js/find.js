0ff4609bbdfcc0d14039afa57f820762f4cfb176
MaxWofford <max.speed.wofford@gmail.com>
2014-08-28 15:48:15 -0700

    Temporary divs to fill in

--- www/js/find.js
+++ www/js/find.js
@@ -1,7 +1,7 @@
-var results = []
+var results = [];
 
 function startPageSpecific() {
-	snapshotFirebase(5);
+	snapshotFirebase(100000); //Debugging geolocation is difficult with proxies
 };
 
 Math.toRad = function(radians) {
@@ -9,18 +9,14 @@ Math.toRad = function(radians) {
 };
 
 function coordsToKilometers (locLat, locLng) {
-	
 	var a = 6371; // km
 	var b = Math.toRad(latitude);
 	var c = Math.toRad(locLat);
 	var d = Math.toRad(locLat-latitude);
 	var e = Math.toRad(locLng-longitude);
-
 	var f = Math.sin(d/2) * Math.sin(d/2) + Math.cos(b) * Math.cos(c) * Math.sin(e/2) * Math.sin(e/2);
 	var g = 2 * Math.atan2(Math.sqrt(f), Math.sqrt(1-f));
-
 	var h = a * g;
-	console.log(h);
 	return h;
 }
 
@@ -30,18 +26,25 @@ function kilometersToMiles(kilometers) {
 
 function snapshotFirebase(proximity) {
 	fb.on('value', function (snapshot) {
-		console.log(snapshot.val());
 		for (var location in snapshot.val()) {
 			locLat = snapshot.val()[location].position.split(',')[0];
 			locLng = snapshot.val()[location].position.split(',')[1];
-			var dist = coordsToKilometers (locLat, locLng)
-
+			var dist = coordsToKilometers (locLat, locLng);
 			if (dist <= proximity) {
-				console.log(snapshot.val()[location].position, 'is close to you');
+				console.info(snapshot.val()[location].position, 'is', dist, 'kilometers from you');
 				results.push(dist + ':' + snapshot.val()[location].position);
 			};
 		}
 	}, function (errorObject) {
-		console.log('The read failed: ' + errorObject.code);
+		console.warn('The read failed: ' + errorObject.code);
 	});
 };
+
+function displayList(results){
+	for (var location in results) {
+		var $div = $("<div>");
+		var listDist = results[location].split(':')[0];
+		$div.text(listDist);
+		$('#nearby').append($div);
+	}
+}

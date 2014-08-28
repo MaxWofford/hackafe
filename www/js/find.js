97a24f0f66cb3ef5e43237909f90e242d5a3afec
MaxWofford <max.speed.wofford@gmail.com>
2014-08-27 23:33:58 -0700

    Fixed proximity for kilometers

--- www/js/find.js
+++ www/js/find.js
@@ -1,18 +1,41 @@
+var results = []
+
 function startPageSpecific() {
 };
 
-var results = []
+Math.toRad = function(radians) {
+  return (radians * Math.PI/180);
+};
+
+function coordsToKilometers (locLat, locLng) {
+	
+	var a = 6371; // km
+	var b = Math.toRad(latitude);
+	var c = Math.toRad(locLat);
+	var d = Math.toRad(locLat-latitude);
+	var e = Math.toRad(locLng-longitude);
+
+	var f = Math.sin(d/2) * Math.sin(d/2) +
+        Math.cos(b) * Math.cos(c) *
+        Math.sin(e/2) * Math.sin(e/2);
+	var g = 2 * Math.atan2(Math.sqrt(f), Math.sqrt(1-f));
+
+	var h = a * g;
+	console.log(h);
+	return h
+}
+
 function snapshotFirebase(proximity) {
 	fb.on('value', function (snapshot) {
 		console.log(snapshot.val());
 		for (var location in snapshot.val()) {
 			locLat = snapshot.val()[location].position.split(',')[0];
 			locLng = snapshot.val()[location].position.split(',')[1];
-			distLat = locLat - latitude;
-			distLng = locLng - longitude
-			if (Math.sqrt(distLat + distLng) <= proximity) {
+			var dist = coordsToKilometers (locLat, locLng)
+
+			if (dist <= proximity) {
 				console.log(snapshot.val()[location].position, 'is close to you');
-				results.push(snapshot.val()[name] + ':' + snapshot.val()[location].position);
+				results.push(dist + ':' + snapshot.val()[location].position);
 			};
 		}
 	}, function (errorObject) {

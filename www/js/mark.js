eb6f436ef7e1602153fe8ace9734596be7ab8bc3
MaxWofford <max.speed.wofford@gmail.com>
2014-12-25 14:36:41 -0800

    Mark page has rating slider and transitions to main page when submitted

--- www/js/mark.js
+++ www/js/mark.js
@@ -1,66 +1,113 @@
 //Map related functions
-function generateMap(lat,lng) {
-	map = new GMaps({
-		div: '#map',
-		lat: lat,
-		lng: lng,
-		zoom: 20
-	});
+function generateMap(lat, lng) {
+    map = new GMaps({
+        div: '#map',
+        lat: lat,
+        lng: lng,
+        zoom: 20
+    });
 };
 
-function centerMap(lat,lng) {
-	map.setCenter(lat, lng);
+function centerMap(lat, lng) {
+    map.setCenter(lat, lng);
 };
 
 function updateLocation() {
-	latitude = map.getCenter().lat();
-	longitude = map.getCenter().lng();
+    latitude = map.getCenter().lat();
+    longitude = map.getCenter().lng();
 };
 
 //Notification bar
 function notifyAlert(notifyMessage) {
-	clearNotification();//Clear previous notification
-	console.warn('ERROR: ' + errorMessage);
-	$('#notification').removeClass('hidden').addClass('alert-danger').text('ERROR: ' + notifyMessage);
+    clearNotification(); //Clear previous notification
+    console.warn('ERROR: ' + errorMessage);
+    $('#notification').removeClass('hidden').addClass('alert-danger').text('ERROR: ' + notifyMessage);
 };
 
 function notifyInfo(notifyMessage) {
-	clearNotification();
-	console.warn('NOTIFY: ' + errorMessage);
-	$('#notification').removeClass('hidden').addClass('alert-info').text(notifyMessage);
+    clearNotification();
+    console.warn('NOTIFY: ' + errorMessage);
+    $('#notification').removeClass('hidden').addClass('alert-info').text(notifyMessage);
 };
 
 function clearNotification() {
-	$('#notification').addClass('hidden').removeClass('alert-danger alert-warning alert-info alert-success').text('');
+    $('#notification').addClass('hidden').removeClass('alert-danger alert-warning alert-info alert-success').text('');
 };
 
 //Form related functions 
 function revealForm() {
-	updateLocation();//Get position from map
-	$('#map-container').addClass('hidden');//Hide map
-	$('#form').removeClass('hidden');//Reveal form
+    updateLocation(); //Get position from map
+    $('#map-container').addClass('hidden'); //Hide map
+    $('#form-red').removeClass('hidden'); //Reveal form
+    $('#form-white').removeClass('hidden'); //Reveal form
+    clearNotification();
 };
 
 //Triggered when user clicks the final submit button
 function submitLocation() {
-	name = $('#nameInput').val();
-	comment = $('#commentInput').val();
-	fb.push({name: name, comment: comment, position: latitude + "," + longitude});
-};
+    var rating = getRating() || '';
+    var name = $('#nameInput').val() || '';
+    var comment = $('#commentInput').val() || '';
+    fb.push({
+        name: name,
+        comment: comment,
+        rating: rating,
+        position: latitude + "," + longitude
+    });
+    // notifyInfo('Submitted!');
+    $('#form-red').addClass('hidden'); //Reveal form
+    $('#form-white').addClass('hidden'); //Reveal form
+    clearNotification();
+    successNotification();
+    setTimeout(function() {
+        returnHome();
+    }, 800);
+}
+
+function successNotification() {
+    $('.success-banner').toggleClass('hidden');
+    setTimeout(function() {
+	    $('.check').css('stroke-dashoffset', 0);
+    }, 100);
+}
+
+function returnHome() {
+    $('#home-link')[0].click();
+}
+
+function getRating() {
+    var rating = ''
+    if ($('#option1')[0].checked == true) {
+        rating = 1;
+    };
+    if ($('#option2')[0].checked == true) {
+        rating = 2;
+    };
+    if ($('#option3')[0].checked == true) {
+        rating = 3;
+    };
+    if ($('#option4')[0].checked == true) {
+        rating = 4;
+    };
+    if ($('#option5')[0].checked == true) {
+        rating = 5;
+    };
+    return rating;
+}
 
 //Triggered onLoad
 function startPageSpecific() {
-	$('#loader').addClass('hidden');//Hide loader button
-	$('#map-container').removeClass('hidden');//Reveal map
-	generateMap(0,0);//Generate a map without waiting for coordinates
-	map.setZoom(21);
-	endLoad();
+    $('#loader').addClass('hidden'); //Hide loader button
+    $('#map-container').removeClass('hidden'); //Reveal map
+    generateMap(0, 0); //Generate a map without waiting for coordinates
+    map.setZoom(21);
+    endLoad();
 };
 
 //Triggered when user presses 'mark location'
 function markPressed() {
-	updateLocation();
-	revealForm();
+    updateLocation();
+    revealForm();
 }
 
 /*function showLocations() {
@@ -78,3 +125,7 @@ function markPressed() {
 		});
 	};
 }*/
+
+$(document).ready(function() {
+    $("#radios").radiosToSlider();
+});
